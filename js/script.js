1eff6177146faa4f7543c2fbd6c9293989e85961
maki <mamajohnjohn@gmail.com>
2022-02-15 11:22:55 -0800

    init

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/*
+Change made??
+I don't know...
+Let's try one more time...
+OK... I don't understand...
+*/
+
+let btn = document.querySelector('button');
+let p = document.querySelector('p')
+btn.addEventListener('click', () => {
+    // console.log('Hello');
+    // alert('Hello!');
+    p.innerText = 'Hello!';
+});
